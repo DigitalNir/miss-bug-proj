efb9145e69a39b01826097702ca1a28f64346e65
Nir Cohen <nircohen.biz@gmail.com>
2023-12-19 21:09:39 +0200

    End ex

--- public/services/bug.service.js
+++ public/services/bug.service.js
@@ -1,69 +1,84 @@
-
 import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
 
 const STORAGE_KEY = 'bugDB'
+const BASE_URL = '/api/bug/'
 
-_createBugs()
+// _createBugs()
 
 export const bugService = {
-    query,
-    getById,
-    save,
-    remove,
+  query,
+  getById,
+  save,
+  remove,
+  getDefaultBugFilter,
+  getEmptyBug,
 }
 
-
-function query() {
-    return storageService.query(STORAGE_KEY)
+function query(filterBy) {
+  return axios
+    .get(BASE_URL)
+    .then((res) => res.data)
+    .then((bugs) => {
+      if (filterBy && filterBy.title) {
+        const regExp = new RegExp(filterBy.title)
+        bugs = bugs.filter((bug) => regExp.test(bug.title))
+      }
+      if (filterBy && filterBy.severity) {
+        bugs = bugs.filter((bug) => bug.severity >= filterBy.severity)
+      }
+      return bugs
+    })
 }
+
 function getById(bugId) {
-    return storageService.get(STORAGE_KEY, bugId)
+  return axios.get(BASE_URL + bugId).then((res) => res.data)
 }
 
 function remove(bugId) {
-    return storageService.remove(STORAGE_KEY, bugId)
+  return axios.get(BASE_URL + bugId + '/remove')
 }
 
 function save(bug) {
-    if (bug._id) {
-        return storageService.put(STORAGE_KEY, bug)
-    } else {
-        return storageService.post(STORAGE_KEY, bug)
-    }
+  const url = BASE_URL + 'save'
+  let queryParams = `?title=${bug.title}&description=${bug.description}&severity=${bug.severity}`
+  if (bug._id) queryParams += `&_id=${bug._id}`
+  return axios.get(url + queryParams)
 }
 
+function getEmptyBug(title = '', description = '', severity = 0) {
+  return { _id: '', title, description, severity, createdAt: Date.now() }
+}
 
-
+function getDefaultBugFilter() {
+  return { title: '', severity: '' }
+}
 
 function _createBugs() {
-    let bugs = utilService.loadFromStorage(STORAGE_KEY)
-    if (!bugs || !bugs.length) {
-        bugs = [
-            {
-                title: "Infinite Loop Detected",
-                severity: 4,
-                _id: "1NF1N1T3"
-            },
-            {
-                title: "Keyboard Not Found",
-                severity: 3,
-                _id: "K3YB0RD"
-            },
-            {
-                title: "404 Coffee Not Found",
-                severity: 2,
-                _id: "C0FF33"
-            },
-            {
-                title: "Unexpected Response",
-                severity: 1,
-                _id: "G0053"
-            }
-        ]
-        utilService.saveToStorage(STORAGE_KEY, bugs)
-    }
-
-
-
+  let bugs = utilService.loadFromStorage(STORAGE_KEY)
+  if (!bugs || !bugs.length) {
+    bugs = [
+      {
+        title: 'Infinite Loop Detected',
+        severity: 4,
+        _id: '1NF1N1T3',
+      },
+      {
+        title: 'Keyboard Not Found',
+        severity: 3,
+        _id: 'K3YB0RD',
+      },
+      {
+        title: '404 Coffee Not Found',
+        severity: 2,
+        _id: 'C0FF33',
+      },
+      {
+        title: 'Unexpected Response',
+        severity: 1,
+        _id: 'G0053',
+      },
+    ]
+    utilService.saveToStorage(STORAGE_KEY, bugs)
+  }
 }
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { bugService } from './services/bug.service.js'
 import { loggerService } from './services/logger.service.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
 // Express Config:
 app.use(express.static('public'))
-// app.use(cookieParser())
+app.use(cookieParser())
 
 // app.get('/', (req, res) => res.send('Hello there'))
 
@@ -48,6 +49,21 @@ app.get('/api/bug/save', (req, res) => {
 // Get a specific Bug (READ)
 app.get('/api/bug/:id', (req, res) => {
   const bugId = req.params.id
+  const visitedBugs = req.cookies.visitedBugs
+    ? JSON.parse(req.cookies.visitedBugs)
+    : []
+
+  if (visitedBugs.length >= 3) {
+    return res.status(401).send('Wait for a bit')
+  }
+
+  if (!visitedBugs.includes(bugId)) {
+    visitedBugs.push(bugId)
+    res.cookie('visitedBugs', JSON.stringify(visitedBugs), { maxAge: 7000 })
+  }
+
+  console.log('User visited the following bugs:', visitedBugs)
+
   bugService
     .getById(bugId)
     .then((bug) => res.send(bug))
